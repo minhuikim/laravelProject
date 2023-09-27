4147687d7a6fa7abbb58e1c1f1850be92ca1d747
minhuikim <kmh940@gmail.com>
2023-09-27 13:21:20 +0900

    20230927 에러메세지 출력 추가

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -9,13 +9,25 @@
     <body>
         <div class="container" style="padding:15px">
             <h1 class="text-xl">글쓰기</h1>
-            <form action="/articles" method="POST" class="mt-3">
+            <form action="/articles" method="POST" style="margin-top:3px">
                 <!-- 419(csrf)에러 방지 -->
                 @csrf
                 <!-- <input type="hidden" name="_token" value="<?=csrf_token();?>" /> -->
                 <input type="text" name="body" style="margin-bottom:2px" class="block w-full mb-2 rounded">
-                <button class="py-1 px-3 rounded" style="background-color:black;color:white;text-size:small;">저장하기</botton>
+                <!--                 
+                    body 에러 출력
+                -->
+                @error('body')
+                    <p class="text-xs" style="color:red;margin-bottom:3px"> {{ $message }} </p>
+                @enderror
+                <button class="py-1 px-3 rounded" style="background-color:black;color:white;font-size:small;">저장하기</botton>
             </form>
+            <!-- dd 메소드는 컬렉션의 아이템을 덤프하여 표시하고 스크립트를 종료 
+                 error : https://laravel.com/docs/10.x/blade#validation-errors 
+                    - all : 전체 에러 출력
+                    - any : 에러 유무 확인
+                    - first : 첫번째 에러만 출력-->
+            <!-- {{ dd($errors->all()) }} -->
         </div>
     </body>
 </html>
