75d2f4a89b29ad2e6ed468b0bddbd9db6c77d3a5
minhuikim <kmh940@gmail.com>
2023-09-27 18:04:00 +0900

    20230927 폼 다시 채우기

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -13,7 +13,8 @@
                 <!-- 419(csrf)에러 방지 -->
                 @csrf
                 <!-- <input type="hidden" name="_token" value="<?=csrf_token();?>" /> -->
-                <input type="text" name="body" style="margin-bottom:2px" class="block w-full mb-2 rounded">
+                <!-- old(body) : 유효성 검사에 실패해도 입력한 내용이 삭제되지 않게 해줌 -->
+                <input type="text" name="body" style="margin-bottom:2px" class="block w-full mb-2 rounded" value="{{ old('body') }}">
                 <!--                 
                     body 에러 출력
                 -->
@@ -23,10 +24,13 @@
                 <button class="py-1 px-3 rounded" style="background-color:black;color:white;font-size:small;">저장하기</botton>
             </form>
             <!-- dd 메소드는 컬렉션의 아이템을 덤프하여 표시하고 스크립트를 종료 
-                 error : https://laravel.com/docs/10.x/blade#validation-errors 
-                    - all : 전체 에러 출력
-                    - any : 에러 유무 확인
-                    - first : 첫번째 에러만 출력-->
+            error : https://laravel.com/docs/10.x/blade#validation-errors 
+            - all : 전체 에러 출력
+            - any : 에러 유무 확인
+            - first : 첫번째 에러만 출력-->
+            <!-- {{ dd(old('body')) }} -->
+            <!-- {{ dd(request()->old('body')) }} -->
+            <!-- {{ dd(request()->session()) }} -->
             <!-- {{ dd($errors->all()) }} -->
         </div>
     </body>
