4a772f6069026728d5c2b589929b47da1a0c614f
minhuikim <kmh940@gmail.com>
2023-09-27 21:20:52 +0900

    20230927 db 글저장 테이블 추가

---
+++ database/migrations/2023_09_27_104458_create_articles_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            // ->type(tablename, size)
+            $table->string('body', 255);
+            $table->foreignId('user_id')->index();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('articles');
+    }
+};
