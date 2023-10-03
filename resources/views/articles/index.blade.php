d420e55e09b8ef470ce38b646bf1d430a1ff244f
minhuikim <kmh940@gmail.com>
2023-10-03 19:22:35 +0900

    20231003 blade 사용

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -5,12 +5,28 @@
     <body>
         <div class="container" style="padding:15px">
             <h1 class="text-xl mb-5">글목록</h1> 
-            <?php foreach($articles as $article): ?>
+            @foreach($articles as $article)
+                <!-- {{ $loop->index }} -->
+                <!-- @if($loop->first)
+                    @continue
+                @endif -->
                 <div class="background-white border rounded mb-3 p-3">
-                    <p><?=$article->body;?></p>
-                    <p><?=$article->created_at;?></p>
+                    <!-- echo + js xss공격 방지 -->
+                    <p>{{ $article->body }}</p>
+                    <p>{{ $article->created_at }}</p>
                 </div>
-            <?php endforeach;?>
+            @endforeach
+
+            <!-- @for($i=0; $i < $articles->count(); $i++)
+                <?//로그인안한 사용자에게만 출력?>
+                @guest
+                <div class="background-white border rounded mb-3 p-3">
+                    <p>{{ $articles[$i]->body }}</p>
+                    <p>{{ $articles[$i]->created_at }}</p>
+                </div>
+                @endguest
+            @endfor
+            -->
         </div>
     </body>
 </html>
