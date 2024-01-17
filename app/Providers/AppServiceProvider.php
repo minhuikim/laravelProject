20fa2547ac91e54d3e15da2dd7393500e37d9f54
minhuikim <kmh940@gmail.com>
2024-01-17 23:10:22 +0900

    20240117 엘로퀀트 스트릭트 모드

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        /** @var \Illuminate\Foundation\Application $app */
+        $app = $this->app;
+        Model::shouldBeStrict(!$app->isProduction()); // 의도하지 않은 동작에 에러를 발생시켜줌
     }
 }
